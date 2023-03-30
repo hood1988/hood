88ba15375ef479ed78d54cfe1bae81c01a322727
hood1988 <125805186+hood1988@users.noreply.github.com>
2023-03-30 15:24:50 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let number1 = 22;
+let number2 = 33;
+let sum = number1 + number2;
+console.log(sum);
+export {};
---
+++ index.ts
@@ -0,0 +1,4 @@
+let number1:number=22
+let number2=33
+let sum=number1+number2
+console.log(sum)
